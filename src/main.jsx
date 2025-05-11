7355666543ab2329ba9c40bdcf19ec559f1846c1
Shinkita Negi <shinkita.negi@gmail.com>
2025-05-11 12:53:36 +0530

    correction

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import './index.css';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import HowItWorks from './components/Howitworks';
-import WhyChooseUs from './components/Whychooseus';
+import WhyChooseUs from "./components/WhyChooseUs"; 
+
 import Testimonials from './components/testimonial';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -14,6 +15,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
+<div className="min-h-screen bg-white dark:bg-black text-black dark:text-white">
       <Header />
       <main className="p-4 text-center">
         <Banner />
@@ -23,6 +25,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      </div>
      
     </>
   );
